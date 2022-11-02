a0a075a437ae5d2dec686d7341c94d379fea383f
Micah Curtis <micahcurtis1997@gmail.com>
2022-11-02 12:30:55 -0700

    Add project files.

---
+++ Curts_LinkedList/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curtis_LinkedList
+{
+    internal class LinkedList
+    {
+        Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public void Add(string data)
+        {
+            Node node = new Node(data);
+            node.Next = head;
+            head = node;
+        }
+
+        public void Print()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public void Contains()
+        {
+
+        }
+    }
+
+}
---
+++ Curts_LinkedList/Program.cs
@@ -0,0 +1,44 @@
+﻿using Curtis_LinkedList;
+
+// NAME: Micah Curtis  
+// COURSE: IT113
+// Behaviors Not Implemented: The Remove and Contains functions are empty and the program will do nothing but give a menu
+ConsoleKeyInfo input;
+LinkedList MyList = new LinkedList();
+Menu();
+
+do
+{
+    input = Console.ReadKey();
+    if (input.Key == ConsoleKey.D1)
+    {
+        //Add
+    }
+    else if (input.Key == ConsoleKey.D2)
+    {
+        //Remove
+    }
+    else if (input.Key == ConsoleKey.D3)
+    {
+        //Search
+    }
+    else if (input.Key == ConsoleKey.D4)
+    {
+        //Print
+    }
+    else
+    {
+        Console.Clear();
+    }
+    Menu();
+} while (input.Key != ConsoleKey.D5);
+
+static void Menu()
+{
+    Console.WriteLine("");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Remove");
+    Console.WriteLine("3. Search");
+    Console.WriteLine("4. Print");
+    Console.WriteLine("5. Exit");
+}
